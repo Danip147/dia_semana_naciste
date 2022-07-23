188331626bbe9422edb075265f3ca0ba6c86102d
Danip147 <danielporras147@outlook.es>
2022-07-23 11:40:29 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+//Saber en que año naciste
+
+
+var año = prompt("¿En que año naciste?"); //lo que los usuarios introducen
+var mes = prompt("¿En que mes naciste?");
+var dia = prompt("¿En que dia naciste?");
+
+var fechaCompleta = (año + (",") + mes  + (",") + dia);
+
+//objeto dias
+const days = {
+    0: 'Domingo',
+    1: 'Lunes',
+    2: 'Martes',
+    3: 'Miercoles',
+    4: 'Jueves',
+    5: 'Viernes',
+    6: 'Sábado',
+}
+
+let birthday = new Date (fechaCompleta);
+
+document.getElementById("dia").innerHTML = (days[birthday.getDay()]);
+
+
+
+
+
